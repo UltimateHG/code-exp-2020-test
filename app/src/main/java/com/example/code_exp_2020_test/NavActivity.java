dfcf6222b2933250d7dc2812662131bc98f32235
xplus2g4 <jackiechenjx@gmail.com>
2020-06-03 19:51:32 +0800

    Create Navigation ViewModel

--- app/src/main/java/com/example/code_exp_2020_test/NavActivity.java
+++ app/src/main/java/com/example/code_exp_2020_test/NavActivity.java
@@ -4,36 +4,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.FirebaseFirestore;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+
+
 public class NavActivity extends AppCompatActivity {
 
-    private Toolbar mainToolbar;
-    private FirebaseAuth.AuthStateListener authStateListener;
-    private static FirebaseAuth mAuth;
-    private FirebaseFirestore firebaseFirestore;
-
-    private String current_user_id;
-
-    private FloatingActionButton addPostBtn;
+    private NavViewModel navViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        navViewModel = new ViewModelProvider(this).get(NavViewModel.class);
+
         setContentView(R.layout.activity_nav);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -44,41 +42,31 @@ public class NavActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        //Define variables
-        mAuth = FirebaseAuth.getInstance();
-        firebaseFirestore = FirebaseFirestore.getInstance();
-
-        //Handle main menu fragment + check if user has logged out
-        if(mAuth.getCurrentUser() != null) {
-
-
-            //Auth state listener, on auth state change, check if user is now null, then go back to LoginActivity
-            authStateListener = (FirebaseAuth.AuthStateListener) (firebaseAuth) -> {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user == null) {
-                    startActivity(new Intent(NavActivity.this, LoginActivity.class));
-                    finish();
-                }
-            };
-
-            //Handle add post button
-            addPostBtn = findViewById(R.id.add_post_btn);
-            addPostBtn.setOnClickListener((v) -> {
-                Intent newPostIntent = new Intent(NavActivity.this, NewPostActivity.class);
-                startActivity(newPostIntent);
-            });
-        }
-
-        //Set toolbar name
-//        mainToolbar = (Toolbar) findViewById(R.id.main_toolbar);
-//        setActionBar(mainToolbar);
-        getSupportActionBar().setTitle("FakeMeh?");
+        setupObservers();
+        setupViews();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(authStateListener);
+    private void setupViews() {
+        //Handle add post button
+        FloatingActionButton addPostBtn = findViewById(R.id.add_post_btn);
+        addPostBtn.setOnClickListener((v) -> {
+            if (navViewModel.getFirebaseUser().getValue() != null) {
+                Intent newPostIntent = new Intent(NavActivity.this, NewPostActivity.class);
+                startActivity(newPostIntent);
+            }
+        });
+    }
+
+    private void setupObservers() {
+        // Create the observer which redirect to login when the user is not logged in.
+        navViewModel.getFirebaseUser().observe(this, new Observer<FirebaseUser>() {
+            @Override
+            public void onChanged(@Nullable FirebaseUser user) {
+                if (user == null) {
+                    startActivity(new Intent(NavActivity.this, LoginActivity.class));
+                }
+            }
+        });
     }
 
     @Override
@@ -100,20 +88,8 @@ public class NavActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        if(authStateListener!=null) mAuth.removeAuthStateListener(authStateListener);
-    }
-
     private void logout() {
-        mAuth.signOut();
-        sendToLogin();
-    }
-
-    private void sendToLogin() {
-        Intent intent = new Intent(NavActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        navViewModel.getFirebaseAuth().signOut();
+        navViewModel.refreshFirebaseUser();
     }
 }
