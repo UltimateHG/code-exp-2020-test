1a330d476edb54f21b1661d6ec558d1cd8bdac6f
xplus2g4 <jackiechenjx@gmail.com>
2020-06-03 19:51:32 +0800

    Create Navigation ViewModel

--- app/src/main/java/com/example/code_exp_2020_test/NavViewModel.java
+++ app/src/main/java/com/example/code_exp_2020_test/NavViewModel.java
@@ -5,17 +5,22 @@ import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class NavViewModel extends ViewModel {
 
-    private MutableLiveData<FirebaseAuth> mAuth;
+    private MutableLiveData<FirebaseUser> mUser;
+    private FirebaseAuth mAuth;
 
     public NavViewModel() {
-        mAuth = new MutableLiveData<>();
-        mAuth.setValue(FirebaseAuth.getInstance());
+        mAuth = FirebaseAuth.getInstance();
+        mUser = new MutableLiveData<>();
+        mUser.setValue(mAuth.getCurrentUser());
     }
 
-    public LiveData<FirebaseAuth> getFirebaseAuth() {
-        return mAuth;
+    public LiveData<FirebaseUser> getFirebaseUser() {
+        return mUser;
     }
+    public void refreshFirebaseUser() { mUser.setValue(mAuth.getCurrentUser()); }
+    public FirebaseAuth getFirebaseAuth() { return mAuth; }
 }
