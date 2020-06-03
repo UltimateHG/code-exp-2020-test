1b38a47920a6982426c8a68929101496a3bfb064
BlazeChron <jasonjhl20@gmail.com>
2020-06-03 15:02:49 +0800

    Added news fragment and respective stuffs for a RecyclerView

---
+++ app/src/main/java/com/example/code_exp_2020_test/NewsRecyclerAdapter.java
@@ -0,0 +1,84 @@
+package com.example.code_exp_2020_test;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import java.util.Date;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NewsRecyclerAdapter extends RecyclerView.Adapter<NewsRecyclerAdapter.ViewHolder> {
+
+    //list of posts and context
+    public List<NewsPost> news_list;
+    public Context context;
+
+    public NewsRecyclerAdapter(List<NewsPost> news_list) {
+        this.news_list = news_list;
+    }
+
+    @Override
+    public NewsRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //create view, set viewholder params
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.news_list_item, parent, false);
+        context = parent.getContext();
+        return new ViewHolder(view);
+    }
+
+    public void onBindViewHolder(final NewsRecyclerAdapter.ViewHolder holder, int position) {
+        //not recyclable
+        holder.setIsRecyclable(false);
+        //initialize variables
+        final String blogPostId = news_list.get(position).NewsPostId;
+
+
+        //set title and link
+        String title = news_list.get(position).getTitle();
+        String body = news_list.get(position).getLink();
+        holder.setTitleText(title);
+        holder.setLinkText(body);
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {return news_list.size();}
+
+    //Custom viewholder class with helper functions
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private View view;
+        private TextView newsTitle;
+        private TextView newsBody;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+
+        public void setTitleText(String titleText) {
+            newsTitle = view.findViewById(R.id.news_title);
+            newsTitle.setText(titleText);
+        }
+
+        public void setLinkText(String bodyText) {
+            newsBody = view.findViewById(R.id.news_link);
+            newsBody.setText(bodyText);
+
+            //supposedly allows hyperlink, might need to edit string
+            newsBody.setMovementMethod(LinkMovementMethod.getInstance());
+        }
+
+    }
+}
