8f639df0799793c555bf3e6099e0c2bcd935e649
xplus2g4 <jackiechenjx@gmail.com>
2020-06-03 17:31:20 +0800

    Add Nav Bar

--- app/src/main/java/com/example/code_exp_2020_test/LoginActivity.java
+++ app/src/main/java/com/example/code_exp_2020_test/LoginActivity.java
@@ -40,7 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         try {
             if (!TextUtils.isEmpty(user.getDisplayName())) {
-                Intent intent = new Intent(this, MainActivity.class);
+                Intent intent = new Intent(this, NavActivity.class);
                 startActivity(intent);
                 finish();
             }
@@ -89,7 +89,7 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.makeText(LoginActivity.this, "Logged in!",
                                         Toast.LENGTH_SHORT).show();
 
-                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                Intent intent = new Intent(getApplicationContext(), NavActivity.class);
                                 startActivity(intent);
                                 finish();
                             } else {
